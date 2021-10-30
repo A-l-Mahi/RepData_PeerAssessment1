f2bc855515772201841f1af57d35bf3dc4c04175
Mahi Aminu Aliyu <mahigital@gmail.com>
2021-10-30 18:36:15 +0100

    analysis phase

--- analysis.R
+++ analysis.R
@@ -1,15 +1,18 @@
+library(impute)
 library(ggplot2)
 library(lubridate)
+library(Hmisc)
+
 
 if(!file.exists("data")){
         download.file(
                 "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip",
-                      destfile = "data/activity.csv"
+                      destfile = "activity.csv"
                 )
 }
 
-data <- read.csv("data/activity.csv")
-
+data <- read.csv("activity.csv")
+data
 data$date <- ymd(data$date)
 
 stepday <- tapply(data$steps, data$date, sum, na.rm = T)
@@ -17,3 +20,17 @@ qplot(stepday, xlab = "number of steps each day", ylab = "Frequency")
 
 meanSteps <- mean(stepday)
 mediansteps <- median(stepday)
+
+avgday <- aggregate(data$steps, by = list(data$interval), mean, na.rm = T)
+
+plot(avgday, type = "l", xlab = "5-minute interval", 
+     ylab = "Average Number of steps", col = "dark red")
+
+
+max(avgday[,1])
+
+length(which(is.na(data)))
+
+data$steps <- impute(data$steps, median)
+
+
